2626e87fcbda4c5e9a4db366166322e05a5e66a3
Simon O'Brien <61605711+sim0wn@users.noreply.github.com>
2024-05-06 12:54:02 -0300

    adding navbar link to home

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,7 +2,10 @@ import Link from "next/link"
 
 export default function Navbar() {
   return (
-    <nav className="border-b px-2 py-4 mx-1.5 dark:border-neutral-800">
+    <nav className="border-b px-2 flex py-4 justify-center mx-1.5 dark:border-neutral-800">
+      <Link href={"/"} className="flex-1 flex text-2xl font-extrabold">
+        ~/
+      </Link>
       <menu className="flex gap-2 justify-end *:border-b *:px-2 py-1 dark:*:border-neutral-800">
         <li>
           <Link href={"#"}>Soluções</Link>
