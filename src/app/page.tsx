530fd66ff5dabf625af1d4cbf21a8032633d622f
Simon O'Brien <61605711+sim0wn@users.noreply.github.com>
2024-05-06 00:28:11 -0300

    adding index route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,91 @@
-import Image from "next/image";
+import EmailIcon from "@/icons/email.icon"
+import GithubIcon from "@/icons/github.icon"
+import HackTheBoxIcon from "@/icons/hackthebox.icon"
+import LattesIcon from "@/icons/lattes.icon"
+import LinkedInIcon from "@/icons/linkedin.icon"
+import TryHackMeIcon from "@/icons/tryhackme.icon"
+import Image from "next/image"
+import Link from "next/link"
 
-export default function Home() {
+export default function WhoAmI() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <main className="flex flex-col sm:flex-row place-items-center justify-center gap-4 h-screen p-1.5">
+      <section className="flex flex-col gap-1.5">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={"/images/sim0wn.jpg"}
+          className="rounded-full"
+          alt=""
+          width={250}
+          height={250}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <ul className="border-y border-neutral-800 py-2 text-4xl flex gap-1.5 justify-center">
+          <li>
+            <Link
+              href={"https://www.linkedin.com/in/halissoncruz/"}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <LinkedInIcon />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"http://lattes.cnpq.br/4781391320784524/"}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <LattesIcon />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"https://app.hackthebox.com/profile/143157/"}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <HackTheBoxIcon />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"https://github.com/sim0wn/"}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <GithubIcon />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"https://tryhackme.com/p/sim0wn/"}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <TryHackMeIcon />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"mailto:contact@sim0wn.com"}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <EmailIcon />
+            </Link>
+          </li>
+        </ul>
+      </section>
+      <section className="max-w-lg">
+        <p className="text-wrap text-lg text-center">
+          Olá, amigo. Seja bem-vindo! Sou um entusiasta apaixonado por Segurança
+          da Informação. Aqui, lhe disponibilizo os links de acesso ao meu
+          currículo acadêmico e profissional, além do meu perfil do LinkedIn e
+          plataformas de desafios. Em breve, estarei disponibilizando resoluções
+          de desafios através dessa plataforma para auxiliar outros estudantes e
+          profissionais de Segurança da Informação a resolverem desafios ou
+          aprenderem coisas novas.
+        </p>
+      </section>
     </main>
-  );
+  )
 }
