b1b93533f105324b86b425f6841b9b43239efa0e
Simon O'Brien <61605711+sim0wn@users.noreply.github.com>
2024-05-08 17:27:42 -0300

    fixing navbar links

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 export default function Navbar() {
   return (
     <nav className="border-b px-2 flex py-4 justify-center mx-1.5 dark:border-neutral-800">
-      <Link href={"/"} className="flex-1 flex text-2xl">
+      <Link href={"/"} className="flex text-2xl">
         ~/
       </Link>
-      <menu className="flex gap-2 justify-end *:border-b *:px-2 py-1 dark:*:border-neutral-800">
+      <menu className="flex flex-1 gap-2 justify-end *:border-b *:px-2 py-1 dark:*:border-neutral-800">
         <li>
           <Link href={"/solutions"}>~/soluções</Link>
         </li>
