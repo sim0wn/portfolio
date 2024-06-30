55db35a622395af5eaa5f2d9d250477a650d2b0f
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-06-29 21:58:17 -0300

    improving pitch text on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,13 @@ import Link from "next/link"
 
 export default function WhoAmI() {
   return (
-    <main className="flex flex-col sm:flex-row place-items-center justify-center gap-4 p-1.5">
+    /* main container */
+    <main className="flex flex-col place-items-center justify-center gap-4 p-1.5">
+      {/* image and social media */}
       <section className="flex flex-col gap-1.5">
         <Image
-          alt=""
-          className="rounded-full"
+          alt="Imagem produzida por inteligência artificial. Um esquilo roxo de capuz com um olhar sério e um fundo escuro."
+          className="rounded-full border border-neutral-800"
           height={250}
           src={"/images/sim0wn.jpg"}
           width={250}
@@ -75,13 +77,16 @@ export default function WhoAmI() {
           </li>
         </ul>
       </section>
-      <section className="max-w-lg">
-        <section className="bg-neutral-800 py-2 px-4 flex rounded-tr-xl rounded-tl-lg select-none">
+      {/* presentation terminal */}
+      <section className="max-w-4xl">
+        {/* application bar */}
+        <header className="bg-neutral-800 py-2 px-4 flex rounded-t-xl select-none">
           <p className="text-center flex-1">Terminal</p>
           <p>x</p>
-        </section>
-        <section className="bg-neutral-900 p-1.5">
-          <header className="inline-flex">
+        </header>
+        <section className="bg-neutral-900 p-1.5 rounded-b-sm">
+          {/* command line */}
+          <header className="inline-flex select-none">
             <p className="text-green-500">root</p>
             <p className="text-yellow-400">@</p>
             <p className="text-blue-600">sim0wn</p>
@@ -89,15 +94,41 @@ export default function WhoAmI() {
             <p className="text-red-600">#</p>
             <p className="italic text-neutral-200 px-1">cat ./welcome.txt</p>
           </header>
-          <p className="text-wrap text-justify">
-            Olá, amigo. Seja bem-vindo! Sou um entusiasta apaixonado por
-            Segurança da Informação. Aqui, lhe disponibilizo os links de acesso
-            ao meu currículo acadêmico e profissional, além do meu perfil do
-            LinkedIn e plataformas de desafios. Em breve, estarei
-            disponibilizando resoluções de desafios através dessa plataforma
-            para auxiliar outros estudantes e profissionais de Segurança da
-            Informação a resolverem desafios ou aprenderem coisas novas.
-          </p>
+          {/* pitch text */}
+          <section className="*:indent-8">
+            <p>
+              Olá, amigo! Sou um Técnico em Informática e estudante de Segurança
+              da Informação. Comecei a programar aos 11 anos, desenvolvendo{" "}
+              <i>plugins</i> para servidores de Minecraft em Java.
+            </p>
+            <p>
+              Desde então, me aprofundei em diversas áreas da computação, com
+              foco em Segurança da Informação. Estou em constante aprendizado,
+              participando de cursos, eventos e desafios de{" "}
+              <i>capture the flag</i>. Recentemente, conquistei a 1ª colocação
+              no CTF Hackers do Bem.
+            </p>
+            <p>
+              Possuo experiência como programador <i>web</i> <i>front-end</i> e{" "}
+              <i>back-end</i> com Next.js e Elysia, além de desenvolvimento de
+              REST API em TypeScript. Tenho experiência prática de mais de 5
+              anos com Linux, incluindo a manutenção e administração do sistema
+              por linha de comando.
+            </p>
+            <p>
+              Adicionalmente, tenho conhecimento em Python e BASH. Atualmente,
+              estou me especializando em Segurança da Informação, com foco em
+              testes de intrusão e exploração de vulnerabilidades em aplicações{" "}
+              <i>web</i>.
+            </p>
+            <p>
+              Por fim, desenvolvi esse projeto para compartilhar meu
+              conhecimento e experiência. Estou ansioso para discutir
+              oportunidades e projetos em comum, além de trocar conhecimento.
+              Você pode encontrar meus perfis em redes sociais e plataformas de
+              CTF logo acima!
+            </p>
+          </section>
         </section>
       </section>
     </main>
