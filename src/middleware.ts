f78574ac0b2b5f2c72e89f7d2deaa92b73f57fa2
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-08-30 18:07:36 -0300

    :bug: fix: returning the modified request to redirect to the localized domain

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest } from "next/server"
 
 export function middleware(request: NextRequest) {
   const { hostname } = request.nextUrl
@@ -17,7 +17,9 @@ export function middleware(request: NextRequest) {
   } else if (hostname.endsWith(".com.br")) {
     request.headers.set("Accept-Language", "pt-BR")
   }
-  return NextResponse.next()
+
+  // return the modified request
+  return request
 }
 
 export const config = {
