f557f5ff4cca6c0bd403a6919712611959854967
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-09-07 02:21:20 -0300

    :sparkles: feat: adding a quotation on index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,21 +9,35 @@ export default async function Homepage() {
   const translation = await getTranslation(getLocale())
   return (
     /* main container */
-    <main className="flex sm:flex-row-reverse flex-col place-items-center justify-center gap-4 p-2">
+    <main className="flex flex-col place-content-center gap-4 p-2">
       {/* logo */}
-      <Image
-        alt={translation.alt.avatar}
-        className="rounded-full"
-        height="300"
-        src="/images/mascot.svg"
-        width="300"
-      />
-      <aside className="max-w-lg">
-        <h1 className="font-bold">{translation.homepage.greeting}</h1>
-        <p className={classNames(jetbrains_mono.className, "font-light")}>
-          {translation.homepage.bio}
-        </p>
-      </aside>
+      <article className="flex flex-col md:flex-row-reverse md:mx-auto gap-2 items-center">
+        <Image
+          alt={translation.alt.avatar}
+          className="rounded-full"
+          height="300"
+          src="/images/mascot.svg"
+          width="300"
+        />
+        <section className="md:max-w-lg">
+          <h1 className="font-bold self-start">{translation.salutation}</h1>
+          <p
+            className={classNames(
+              jetbrains_mono.className,
+              "font-light text-justify",
+            )}
+          >
+            {translation.bio}
+          </p>
+        </section>
+      </article>
+      <blockquote className="text-sm md:max-w-2xl md:mx-auto">
+        <p className="text-center">&quot;{translation.quote.message}&quot;</p>
+        <footer className="text-right">
+          — {translation.quote.author},{" "}
+          <cite>What it means to be an intellectual</cite>
+        </footer>
+      </blockquote>
     </main>
   )
 }
