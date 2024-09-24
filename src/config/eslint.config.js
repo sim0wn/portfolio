9adc3b155d451e598bc55c1a043d2197669546d9
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-09-24 12:42:44 -0300

    :arrow_down: downgrading ESLint version to fix deploy

---
+++ src/config/eslint.config.js
@@ -0,0 +1,32 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import next from 'eslint-config-next';
+import prettier from 'eslint-config-prettier';
+import perfectionist from 'eslint-plugin-perfectionist';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.url,
+});
+
+export default [
+  js.configs.recommended,
+  next.coreWebVitals,
+  perfectionist.configs.recommendedNatural,
+  prettier,
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      // Add any custom rules here
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+  },
+];
