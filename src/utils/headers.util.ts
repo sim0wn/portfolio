ddea04b29bb7e4090775f18436aba7edc636161e
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-10-07 20:47:50 -0300

    :truck: chore: rearranging utilitaries

--- src/utils/headers.util.ts
+++ src/utils/headers.util.ts
@@ -1,6 +1,11 @@
 import { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers"
 
-// transform the headers into a simple object
+/**
+ * Parses the given headers and returns them as a record of key-value pairs.
+ *
+ * @param headers - The headers to parse, provided as a ReadonlyHeaders object.
+ * @returns A record where each key is a header name and each value is the corresponding header value.
+ */
 export function parseHeaders(headers: ReadonlyHeaders): Record<string, string> {
   const negotiatorHeaders: Record<string, string> = {}
   headers.forEach((value: string, key: string) => {
---
+++ src/utils/locale.util.ts
@@ -0,0 +1,37 @@
+import { i18n } from "@/config/i18n.config"
+import { match } from "@formatjs/intl-localematcher"
+import Negotiator from "negotiator"
+import { headers } from "next/headers"
+
+import { parseHeaders } from "./headers.util"
+
+/**
+ * Retrieves the best matching locale based on the preferred languages
+ * from the request headers.
+ *
+ * This function uses the `negotiator` and `intl-localematcher` libraries
+ * to determine the most appropriate locale from the available locales.
+ *
+ * @returns {string} The best matching locale.
+ */
+export function getLocale() {
+  const { defaultLocale, locales } = i18n
+  const preferredLanguages = new Negotiator({
+    headers: parseHeaders(headers()),
+  }).languages(locales)
+  return match(preferredLanguages, locales, defaultLocale)
+}
+
+/**
+ * Returns the domain associated with the given locale.
+ *
+ * @param locale - The locale string (e.g., "pt-BR", "en-US").
+ * @returns The domain corresponding to the provided locale.
+ */
+export function getLocaleDomain(locale: string) {
+  if (locale === "pt-BR") {
+    return "sim0wn.com.br"
+  } else if (locale === "en-US") {
+    return "sim0wn.com"
+  }
+}
