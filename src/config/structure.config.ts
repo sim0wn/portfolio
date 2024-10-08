de07f46699d36416c78f95ef8312c325151fac3f
sim0wn <61605711+sim0wn@users.noreply.github.com>
2024-10-07 22:01:44 -0300

    :wrench: chore: changing structure for Sanity Studio

--- src/config/structure.config.ts
+++ src/config/structure.config.ts
@@ -3,15 +3,48 @@ import type { StructureResolver } from "sanity/structure"
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title("Blog")
+    .id("content")
+    .title("Content")
     .items([
-      S.documentTypeListItem("article").title("Articles"),
-      S.documentTypeListItem("category").title("Categories"),
-      S.documentTypeListItem("author").title("Authors"),
+      S.listItem()
+        .title("Blog")
+        .child(
+          S.list()
+            .id("blog")
+            .items([
+              S.documentTypeListItem("article").title("Articles"),
+              S.documentTypeListItem("category").title("Categories"),
+              S.documentTypeListItem("author").title("Authors"),
+            ]),
+        ),
+      S.listItem()
+        .title("Portfolio")
+        .child(
+          S.list()
+            .id("portfolio")
+            .items([
+              S.documentTypeListItem("testimonial").title("Testimonials"),
+              S.documentTypeListItem("service").title("Services"),
+            ]),
+        ),
       S.divider(),
-      ...S.documentTypeListItems().filter(
-        (item) =>
-          item.getId() &&
-          !["article", "category", "author"].includes(item.getId()!),
-      ),
+      S.listItem()
+        .title("Other")
+        .child(
+          S.list()
+            .id("other")
+            .items([
+              ...S.documentTypeListItems().filter(
+                (item) =>
+                  item.getId() &&
+                  ![
+                    "article",
+                    "category",
+                    "author",
+                    "testimonial",
+                    "service",
+                  ].includes(item.getId()!),
+              ),
+            ]),
+        ),
     ])
