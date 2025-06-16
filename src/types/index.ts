0daed69c91fa69b91a702c5107629931621938ee
sim0wn <61605711+sim0wn@users.noreply.github.com>
2025-06-16 20:47:53 -0300

    :boom: fix: unused type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,5 @@ export {
   type ContactFormData,
   type ContactFormState,
 } from "./contact-form.type"
-export { type Locale } from "./locale.type"
 export { type NestedDocs } from "./nested-docs.type"
 export * from "./payload.type"
--- src/types/locale.type.ts
+++
@@ -1,7 +0,0 @@
-import { i18n } from "@/config"
-
-/**
- * Represents a locale type derived from the `i18n` object's `locales` array.
- * This type is used to ensure that only valid locales defined in the `i18n.locales` array are used.
- */
-export type Locale = (typeof i18n)["locales"][number]
