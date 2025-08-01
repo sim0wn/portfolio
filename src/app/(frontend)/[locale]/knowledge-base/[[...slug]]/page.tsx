00e5610e871d3119d27ce72c1182a83a9ae8f0bb
sim0wn <61605711+sim0wn@users.noreply.github.com>
2025-08-01 01:37:11 -0300

    fix: generate dynamic params

--- src/app/(frontend)/[locale]/knowledge-base/[[...slug]]/page.tsx
+++ src/app/(frontend)/[locale]/knowledge-base/[[...slug]]/page.tsx
@@ -9,10 +9,12 @@ import {
   CardTitle,
   RichText,
 } from "@/components"
-import { Link, routing } from "@/i18n"
+import { Link } from "@/i18n"
 import { payload } from "@/lib"
 
 type Props = { params: Promise<{ locale: Locale; slug: string[] }> }
+
+export const dynamicParams = true
 
 export async function generateStaticParams() {
   const { docs: pages } = await payload.find({
@@ -25,19 +27,13 @@ export async function generateStaticParams() {
     },
   })
 
-  return [
-    ...pages.flatMap(({ url }) => {
-      if (!(url && typeof url === "object")) return null
-      return Object.entries(url).map(([locale, slugs]) => ({
-        locale,
-        slug: (slugs as string).split("/"),
-      }))
-    }),
-    routing.locales.map((locale) => ({
+  return pages.flatMap(({ url }) => {
+    if (!(url && typeof url === "object")) return null
+    return Object.entries(url).map(([locale, slugs]) => ({
       locale,
-      slug: [],
-    })),
-  ]
+      slug: (slugs as string).split("/"),
+    }))
+  })
 }
 
 export default async function Page({ params }: Props) {
